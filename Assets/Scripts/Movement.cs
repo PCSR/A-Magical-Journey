4d1af95bfb0f38078798d797e9366e6123bc0d3c
PCSR <36503238+PCSR@users.noreply.github.com>
2019-05-07 15:57:32 +0100

    Add files via upload

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    bool isJumping;
+    Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         float move = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2 (speed * move, rb.velocity.y);
+        Jump();
+    }
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping){
+            isJumping = true;
+
+            rb.AddForce(new Vector2(rb.velocity.x, jumpForce));
+ 
+        }
+    }
+    
+}
